60ec26f24517afcfaf1af35652a65520e92a03a1
Mike <mike1072@gmail.com>
2014-09-09 23:30:09 -0700

    fixed everything (i hope)

--- Assets/GameData.cs
+++ Assets/GameData.cs
@@ -1,38 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-public class GameData : MonoBehaviour {
+public class GameData : MonoBehaviour
+{
+    public class Player
+    {
+        public int ID { get; private set; }
+        public bool Alive { get; set; }
+        public Player(int id) { this.ID = id; Alive = true; }
+    }
 
-	public int NumPlayers = 2;
-	
-	private bool[] deadPlayers;
+	public short NumPlayers = 2;
+    private List<Player> players;
+
+    public int? winningPlayerID = null;
 
 	// Use this for initialization
-	void Start () {
-		deadPlayers = new bool[NumPlayers];
-		for(int i = 0; i < NumPlayers; i++) {
-			deadPlayers[i] = false;
-		}
+	void Start ()
+    {
+        players = new List<Player>();
+        for (int id = 0; id < NumPlayers; id++)
+        {
+            players.Add(new Player(id));
+        }
+		
+		Object.DontDestroyOnLoad(gameObject);
 	}
 	
-	public void RegisterPlayerDead(int id) {
-		deadPlayers[id] = true;
-		
-		int deadCount = 0;
-		// check if only one left
-		for(int i = 0; i < NumPlayers; i++) {
-			if(deadPlayers[i] == true) deadCount += 1;
-		}
-		
-		Debug.Log ("Dead Count = " + deadCount);
-		
-		if(deadCount >= NumPlayers-1) {
+	public void RegisterPlayerDead(int id)
+    {
+        var player = players.Where(u => u.ID == id).SingleOrDefault();
+        player.Alive = false;
+
+        Debug.Log("Dead Count = " + players.Where(u => !u.Alive).Count());
+        
+		// check if only one player alive
+        var alivePlayers = players.Where(u => u.Alive);
+
+        if (alivePlayers.Count() == 1)
+        {
+            winningPlayerID = alivePlayers.First().ID;
 			Application.LoadLevel("End");
-		}
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 }
